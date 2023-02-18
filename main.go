722a7876a2042c3978b8eb47083b657f2ec003bc
mo1om <liemorris2001@gmail.com>
2023-02-18 16:31:21 +0800

    v1 to api/v1

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	. "api/src"
 	"api/database"
- 
+	. "api/src"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	v1 := router.Group("/v1")
+	v1 := router.Group("api/v1")
 	AddCommentRouter(v1)
 	go func() {
 		database.DBconnect()
--- src/UserRouter.go
+++ src/UserRouter.go
@@ -1,11 +1,15 @@
 package src
+
 import (
-	"github.com/gin-gonic/gin"
 	"api/service"
+
+	"github.com/gin-gonic/gin"
 )
-func AddCommentRouter(r* gin.RouterGroup){
-	comment	:= r.Group("/comments")
-	comment.GET("/",service.FindAllComment)
-	comment.GET("/:id",service.GetUserById)
-	comment.POST("/",service.POSTAllComment)
+
+func AddCommentRouter(r *gin.RouterGroup) {
+	comment := r.Group("/comments")
+	comment.GET("/all", service.FindAllComment)
+	comment.GET("/comment", service.GetCommentById)
+	comment.GET("/serach", service.GetCommentByTeacher)
+	// comment.POST("/", service.POSTAllComment)
 }
