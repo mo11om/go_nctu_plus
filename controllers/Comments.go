43f2f62906de323ef1eafbc885860fec92f502b1
mo1om <MY_NAME@example.com>
2023-03-09 01:05:52 +0800

    search slow sql finish need cache

--- controllers/Comments.go
+++ controllers/Comments.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"api/database"
-	"strconv"
 	"time"
 )
 
@@ -15,43 +14,88 @@ type Comment struct {
 	Id                  int       `json:"id"`
 	UserId              int       `json:"-"`
 	Courseteachershipid int       `gorm:"column:course_teachership_id"  json:"-"`
-	Content             string    `gorm:"content" json:"Content" `
+	Content             string    `gorm:"content" json:"-" `
 	Is_anonymous        bool      `gorm:"is_anonymous" json:"is_anonymous"`
+	Name                string    `gorm:"name" json "name"`
+	Ch_name             string    `gorm:"ch_name" json "ch_name"`
 	Title               string    `gorm:"title" json:"title"`
 	Updated_at          time.Time `gorm:"updated_at" json:"updated_at"`
 	Created_at          time.Time `gorm:"created_at" json:"created_at"`
 }
-type Teachers struct {
-	Name       string    `gorm:"name" json "name"`
-	real_id    string    `gorm:"real_id" json:"real_id"`
-	Is_deleted bool      `gorm:"is_deleted" json:"is_deleted"`
-	Id         int       `json:"id"`
-	Updated_at time.Time `gorm:"updated_at" json:"updated_at"`
-	Created_at time.Time `gorm:"created_at" json:"created_at"`
-}
 
-func FindTeacher(question string) Teachers {
+// func FindTeacher(question string) []Teachers {
 
-	var teacher Teachers
+// 	var teacher []Teachers
 
-	// var send []Comment
+// 	// var send []Comment
 
-	database.Db.Raw("SELECT * FROM    teachers where (name =? );",
-		question).Scan(&teacher)
+// 	database.Db.Raw("SELECT * FROM    teachers where (name =? );",
+// 		question).Scan(&teacher)
 
-	return teacher
-}
-
-func FindCommentByQuestion(question string) []Comment {
+// 	return teacher
+// }
+func FindCommentByTeacher(teacher string) []Comment {
 	var c []Comment
 	// var send []Comment
 
-	teacher := FindTeacher(question)
-	teacher_query := "[" + strconv.Itoa(teacher.Id) + "]"
-	database.Db.Raw("select * from discusses where course_teachership_id in (select id from course_teacherships where  course_teacherships .teacher_id  =  ?);",
-		teacher_query).Scan(&c)
+	database.Db.Raw("SELECT  discusses.*,   teachers.name , courses.ch_name FROM  course_teacherships as ct INNER JOIN courses ON courses.id = ct.course_id		INNER JOIN discusses  ON       discusses .course_teachership_id = ct.id 		INNER JOIN teachers ON   ct.teacher_id LIKE CONCAT('[', teachers.id, ']')		where(		  teachers.name =  ?		) limit 20",
+		teacher).Scan(&c)
 
 	return c
+}
+
+func FindCommentByChName(ch_name string) []Comment {
+
+	var c []Comment
+	var ch_name_query string = "%" + ch_name + "%"
+	database.Db.Raw("SELECT  discusses.*,   teachers.name , courses.ch_name FROM  course_teacherships as ct INNER JOIN courses ON courses.id = ct.course_id		INNER JOIN discusses  ON       discusses .course_teachership_id = ct.id 		INNER JOIN teachers ON   ct.teacher_id LIKE CONCAT('[', teachers.id, ']')		where(courses. ch_name  like ?) limit 20",
+		ch_name_query).Scan(&c)
+
+	return c
+}
+
+// func FindCommentByTitle(title string) []Comment {
+// 	var c []Comment
+// 	var title_query string = "%" + title + "%"
+// 	database.Db.Raw("SELECT  discusses.*,   teachers.name , courses.ch_name FROM  course_teacherships as ct INNER JOIN courses ON courses.id = ct.course_id		INNER JOIN discusses  ON       discusses .course_teachership_id = ct.id 		INNER JOIN teachers ON   ct.teacher_id LIKE CONCAT('[', teachers.id, ']')		where(		discusses.title like ?	) limit 20",
+// 		title_query).Scan(&c)
+
+// 	return c
+// }
+
+func FindAllCommentsByQuestion(question string) []Comment {
+	// var send []Comment
+	// Create channels to synchronize the goroutines
+	c_teacher_ch := make(chan []Comment)
+	c_chname_ch := make(chan []Comment)
+	//c_title_ch := make(chan []Comment)
+	go func() {
+		c_teacher := FindCommentByTeacher(question)
+		c_teacher_ch <- c_teacher // send c_teacher to the channel when the function completes
+	}()
+	go func() {
+		c_chname := FindCommentByChName(question)
+		c_chname_ch <- c_chname // send c_chname to the channel when the function completes
+	}()
+	// go func() {
+	// 	c_title := FindCommentByTitle(question)
+	// 	c_title_ch <- c_title // send c_chname to the channel when the function completes
+
+	// }()
+
+	// Wait for the two channels to receive values
+	c_teacher := <-c_teacher_ch
+	c_chname := <-c_chname_ch
+	//c_title := <-c_title_ch
+	// var c_teacher []Comment = FindCommentByTeacher(question)
+	// var c_chname []Comment = FindCommentByChName(question)
+	c := (append(c_teacher, c_chname...))
+
+	//c := append(append(c_teacher, c_chname...), c_title...)
+	return c
+}
+func FindCommentByQuestion(question string) []Comment {
+	return FindAllCommentsByQuestion(question)
 }
 
 func FindCommentById(id string) Comment {
