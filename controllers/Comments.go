817a326408ae182a3abc8b4babd7cb3972004f55
mo1om <MY_NAME@example.com>
2023-04-18 11:32:31 +0800

    delete unnecessary

--- controllers/Comments.go
+++ controllers/Comments.go
@@ -99,7 +99,7 @@ func FindCommentById(id string) Comment {
 func CommentLimitOffset(limit, page int) ([]Comment, error) {
 	var c []Comment
 	query := `
-	SELECT discusses.id, discusses.title, discusses.created_at, teachers.name, courses.ch_name
+	SELECT discusses.id, discusses.title , teachers.name, courses.ch_name
 	FROM course_teacherships as ct
 	INNER JOIN courses ON courses.id = ct.course_id
 	INNER JOIN discusses ON discusses.course_teachership_id = ct.id
